e35052db6e38acb1e27a866a82b8096bd6d696b7
Kevin Webber <kevin.webber@lightbend.com>
2016-10-18 18:32:33 -0400

    Minor fix to slack uri

--- src/main/scala/api/AuthTest.scala
+++ src/main/scala/api/AuthTest.scala
@@ -11,7 +11,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class AuthTest(implicit ec: ExecutionContext, mat: ActorMaterializer, s: ActorSystem) extends SlackOutgoing {
 
   def authTest(token: Token): Future[Either[String, AuthTestResponse]] = {
-    val req = HttpRequest().withUri(Uri("https://slack.com/api/auth.test").withQuery(Uri.Query(Map("token" -> token))))
+    val req = HttpRequest().withUri(Uri("/api/auth.test").withQuery(Uri.Query(Map("token" -> token))))
     slackApiRequest(req).flatMap(futureResponse[AuthTestResponse])
   }
 
